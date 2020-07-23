f2d0efe58a5443f10ba5d75dbedcd76adf1affdd
Ganesh <43610605+xq8ed@users.noreply.github.com>
2020-07-24 03:38:26 +0530

    modified main, added 3rd folder
    modified main for passing multiple images, added path for input and output of data, added a new folder - Extracted Data

--- Code/main.py
+++ Code/main.py
@@ -1,47 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 10 16:09:40 2020
+""" main method to execute all functions."""
 
-@author: Ganesh
-"""
-import matplotlib.pyplot as plt
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2 as cv
+import pandas
 from module1 import preprocess, postprocess
-from module2 import feature_extract as fe
+from module2 import shape, margin
 
-def save_image(path, filename, img):
-  if(cv.imwrite(path+filename+'_bw.png', img)==True):
+def save_image(path, img):
+  if(cv.imwrite(path+'_bw.png', img)==True):
     print('Image written succesfully')
   else:
     print('Failed to write Image')
 
-pathRGB = '../Data/Samples/RGB/'
-filename ='leaf_11'
-ext = '.JPG'
+DATA_IN_PATH = "..\\Data\\RGB"
+DATA_OUT_PATH = "..\\Extracted_data"
+IMG_NAME_LIST = [f for dp, dn, filenames in os.walk(DATA_IN_PATH) for f in filenames if os.path.splitext(f)[1].lower() == '.jpg']
 
-img = preprocess.prep(pathRGB, filename, ext)
-fig = plt.figure(figsize=(18,12))
-plt.subplot(221)
-plt.imshow(img, cmap = 'gray')
+shape_vec, margin_vec = [], []
+for IMG_NAME in IMG_NAME_LIST:
+	IMG_PATH = os.path.join(DATA_IN_PATH, IMG_NAME)
+	img = preprocess.prep(IMG_PATH)
+	img = postprocess.postp(img)
+	iname = os.path.splitext(IMG_NAME)[0]
+	save_image(os.path.join(DATA_OUT_PATH, "images", iname), img)
+	shape_vec.append(shape.shape_vector(img))
+	margin_vec.append(margin.margin_vector(img))
 
-img2 = postprocess.postp(img)
-save_image('../Data/Samples/extracted/', filename, img2)
-plt.subplot(222)
-plt.imshow(img2, cmap = 'gray')
-
-norm_shape_vec, interp_shape_vec = fe.feat_ext(img2)
-x = np.arange(1, len(norm_shape_vec)+1)
-y = norm_shape_vec
-plt.subplot(223)
-plt.title('Normalized Signature ('+str(len(norm_shape_vec))+' points)', size=18)
-plt.plot(x, y)
-plt.grid()
-
-x = np.arange(1, len(interp_shape_vec)+1)
-y = interp_shape_vec
-plt.subplot(224)
-plt.title('Interpolated Signature ('+str(len(interp_shape_vec))+' points)', size=18)
-plt.plot(x, y)
-plt.grid()
-plt.show()
+df_shape = pandas.DataFrame(shape_vec, index= IMG_NAME_LIST)
+df_shape.to_csv(os.path.join(DATA_OUT_PATH, "features", "shape.csv"), float_format='%.8f')
+df_margin = pandas.DataFrame(margin_vec, index= IMG_NAME_LIST)
+df_margin.to_csv(os.path.join(DATA_OUT_PATH, "features", "margin.csv"), float_format='%.8f')
--- Code/module1/preprocess.py
+++ Code/module1/preprocess.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-module for preprocessing on raw image
-"""
+""" module for preprocessing on raw image """
 
 import cv2 as cv
 import numpy as np
 
-def prep(path, filename, ext):
-    """ Returns binary image 
-        pass arguments as string
-        'path', 'filename', 'ext'
+def prep(path, flag = False):
+    """
+    Input : path to image file
+    Returns : binary image
     """
 
-    img = cv.imread(path+filename+ext)
+    img = cv.imread(path)
     
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     
@@ -24,7 +21,6 @@ def prep(path, filename, ext):
     blur = cv.GaussianBlur(gray, (5,5), 0)
     #canny = cv.Canny(blur, 50, 100)
     retval, binary = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-    
     
     #--- finding longest contour ---
     contours = cv.findContours(binary.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) [-2] # [-2] is offset used, otherwise drawContours doesn't recognise contour
@@ -39,7 +35,6 @@ def prep(path, filename, ext):
     #print("Cropped mask after applying to binary image :")
     x, y, w, h = cv.boundingRect(contour)
     crop_bin = mask_bin[y-20:y+h+20, x-20:x+w+20]
-    
     
     #--- cropping gray image ---
     #print("Cropped gray image after applying mask :")
